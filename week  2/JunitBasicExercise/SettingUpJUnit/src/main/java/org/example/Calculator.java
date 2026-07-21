f4dfff57eb51292acbae7424773cd9a9ffd8bd1c
adity mittal <keshavaditya351@gmail.com>
2026-07-21 20:02:43 +0530

    Added junit folder from week 2

---
+++ week  2/JunitBasicExercise/SettingUpJUnit/src/main/java/org/example/Calculator.java
@@ -0,0 +1,20 @@
+// src/main/java/com/example/Calculator.java
+package org.example;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
